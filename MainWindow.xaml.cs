dc6a9e63d1b030d9e7fc7d1fcc2a331442c340c4
Seroz <cuileiyao1719@126.com>
2022-03-16 22:06:07 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+
+namespace MultiPlayer
+{
+    public partial class MainWindow : Window
+    {
+        private List<PlayerWindow> playerList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            playerList = new List<PlayerWindow>();
+        }
+
+        ~MainWindow()
+        {
+            foreach (PlayerWindow playerWindow in playerList)
+            {
+                playerWindow.Close();
+            }
+        }
+
+        private void Button_Video1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "mp4文件(*.mp4)|*.mp4";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBox_VIdeo1.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_Video2_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog openFileDialog = new();
+            openFileDialog.Filter = "mp4文件(*.mp4)|*.mp4";
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextBox_VIdeo2.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Button_Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(TextBox_VIdeo1.Text))
+            {
+                MessageBox.Show("Video1 was not found", "Error");
+                return;
+            }
+
+            //if (!File.Exists(TextBox_VIdeo2.Text))
+            //{
+            //    MessageBox.Show("Video2 was not found", "Error");
+            //    return;
+            //}
+
+            PlayerWindow playerWindow1 = new(TextBox_VIdeo1.Text);
+            playerList.Add(playerWindow1);
+            playerWindow1.Show();
+
+            //PlayerWindow playerWindow2 = new(TextBox_VIdeo2.Text);
+            //playerWindow2.Show();
+            //playerList.Add(playerWindow2);
+        }
+
+        private void Button_Abort_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (PlayerWindow playerWindow in playerList)
+            {
+                playerWindow.Close();
+            }
+        }
+
+    }
+}
---
+++ PlayerWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Windows;
+
+namespace MultiPlayer
+{
+    public partial class PlayerWindow : Window
+    {
+        public PlayerWindow(string uri)
+        {
+            InitializeComponent();
+
+            mediaElement.Source = new Uri(uri);
+        }
+        ~PlayerWindow()
+        {
+            mediaElement.Close();
+        }
+
+        private void OnClickPlayMedia(object sender, RoutedEventArgs args)
+        {
+            mediaElement.Play();
+
+            InitializePropertyValues();
+        }
+
+        private void OnClickPauseMedia(object sender, RoutedEventArgs args)
+        {
+            mediaElement.Pause();
+        }
+
+        private void OnClickTopMedia(object sender, RoutedEventArgs e)
+        {
+            Topmost = !Topmost;
+        }
+
+        private void OnClickMinMedia(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void ChangeMediaVolume(object sender, RoutedPropertyChangedEventArgs<double> args)
+        {
+            mediaElement.Volume = (double)volumeSlider.Value;
+        }
+
+        private void ChangeMediaSpeedRatio(object sender, RoutedPropertyChangedEventArgs<double> args)
+        {
+            mediaElement.SpeedRatio = (double)speedRatioSlider.Value;
+        }
+
+        private void Element_MediaOpened(object sender, EventArgs e)
+        {
+            timelineSlider.Maximum = mediaElement.NaturalDuration.TimeSpan.TotalMilliseconds;
+        }
+
+        private void Element_MediaEnded(object sender, EventArgs e)
+        {
+            mediaElement.Stop();
+        }
+
+        private void SeekToMediaPosition(object sender, RoutedPropertyChangedEventArgs<double> args)
+        {
+            int SliderValue = (int)timelineSlider.Value;
+
+            TimeSpan ts = new(0, 0, 0, 0, SliderValue);
+            mediaElement.Position = ts;
+        }
+
+        void InitializePropertyValues()
+        {
+            mediaElement.Volume = (double)volumeSlider.Value;
+            mediaElement.SpeedRatio = (double)speedRatioSlider.Value;
+        }
+    }
+}
